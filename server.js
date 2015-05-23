d8faa8845791ad508c8c245cb0af463ce0daff6f
jkang107 <jkang107@gmail.com>
2015-05-23 23:08:23 +0900

    getTravel

--- server.js
+++ server.js
@@ -19,14 +19,14 @@ app.use(bodyParser.urlencoded({
 app.use(cors());
 
 var mysql = require('mysql');
-var ejs= require('ejs');
+var ejs = require('ejs');
 
 var client = mysql.createConnection({
-	host    :'localhost',
-    port : 3307,
+    host: 'localhost',
+    port: 3307,
     user: 'root',
-	password: '0724',
-	database:'travel_buddy'
+    password: '0724',
+    database: 'travel_buddy'
 });
 
 var port = Number(process.env.PORT || 5000);
@@ -75,99 +75,87 @@ app.post("/sendTravelInfo", function(req, res) {
     res.send("thank you client!");
 
     switch (travelType) {
-    	case "travelWith":
-    		query = client.query("INSERT INTO travel (userId, type, sex, age, when_from, when_to, country_from, comment) VALUES ('" + JSON.stringify(userId) + "', '" 
-							+ JSON.stringify(travelType) + "', '"
-							+ JSON.stringify(userInfo.user_gender) + "', '" 
-							+ JSON.stringify(userInfo.user_age) + "', '" 
-							+ JSON.stringify(travelInfo.date_from) + "', '" 
-							+ JSON.stringify(travelInfo.date_to) + "', '" 
-							+ JSON.stringify(travelInfo.country_from) + "', '" 
-							+ JSON.stringify(travelInfo.comment) + "');");
-    		break;
-    	case "moveWith":
-    		query = client.query("INSERT INTO travel (userId, type, sex, age, when_from, country_from, country_to, city_from, city_to, transportation, comment) VALUES ('" + JSON.stringify(userId) + "', '" 
-							+ JSON.stringify(travelType) + "', '"
-							+ JSON.stringify(userInfo.user_gender) + "', '" 
-							+ JSON.stringify(userInfo.user_age) + "', '" 
-							+ JSON.stringify(travelInfo.date_from) + "', '" 
-							+ JSON.stringify(travelInfo.country_from) + "', '" 
-							+ JSON.stringify(travelInfo.country_to) + "', '" 
-							+ JSON.stringify(travelInfo.city_from) + "', '" 
-							+ JSON.stringify(travelInfo.city_to) + "', '" 
-							+ JSON.stringify(travelInfo.transportation) + "', '" 
-							+ JSON.stringify(travelInfo.comment) + "');");
-    		break;
-    	case "tourWith":
-    		query = client.query("INSERT INTO travel (userId, type, sex, age, when_from, when_to, country_from, tour_name, comment) VALUES ('" + JSON.stringify(userId) + "', '" 
-							+ JSON.stringify(travelType) + "', '"
-							+ JSON.stringify(userInfo.user_gender) + "', '" 
-							+ JSON.stringify(userInfo.user_age) + "', '" 
-							+ JSON.stringify(travelInfo.date_from) + "', '" 
-							+ JSON.stringify(travelInfo.date_to) + "', '" 
-							+ JSON.stringify(travelInfo.country_from) + "', '" 
-							+ JSON.stringify(travelInfo.tour_name) + "', '" 
-							+ JSON.stringify(travelInfo.comment) + "');");
-    		break;
-    	case "foodWith":
-    		query = client.query("INSERT INTO travel (userId, type, sex, age, when_from, country_from, city_from, comment) VALUES ('" + JSON.stringify(userId) + "', '" 
-							+ JSON.stringify(travelType) + "', '"
-							+ JSON.stringify(userInfo.user_gender) + "', '" 
-							+ JSON.stringify(userInfo.user_age) + "', '" 
-							+ JSON.stringify(travelInfo.date_from) + "', '" 
-							+ JSON.stringify(travelInfo.country_from) + "', '" 
-							+ JSON.stringify(travelInfo.city_from) + "', '" 
-							+ JSON.stringify(travelInfo.comment) + "');");
-    		break;
+        case "travelWith":
+            query = client.query("INSERT INTO travel (userId, type, sex, age, when_from, when_to, country_from, comment) VALUES ('" + userId + "', '" + travelType + "', '" + userInfo.user_gender + "', '" + userInfo.user_age + "', '" + travelInfo.date_from + "', '" + travelInfo.date_to + "', '" + travelInfo.country_from + "', '" + travelInfo.comment + "');");
+            break;
+        case "moveWith":
+            query = client.query("INSERT INTO travel (userId, type, sex, age, when_from, country_from, country_to, city_from, city_to, transportation, comment) VALUES ('" + userId + "', '" + travelType + "', '" + userInfo.user_gender + "', '" + userInfo.user_age + "', '" + travelInfo.date_from + "', '" + travelInfo.country_from + "', '" + travelInfo.country_to + "', '" + travelInfo.city_from + "', '" + travelInfo.city_to + "', '" + travelInfo.transportation + "', '" + travelInfo.comment + "');");
+            break;
+        case "tourWith":
+            query = client.query("INSERT INTO travel (userId, type, sex, age, when_from, when_to, country_from, tour_name, comment) VALUES ('" + userId + "', '" + travelType + "', '" + userInfo.user_gender + "', '" + userInfo.user_age + "', '" + travelInfo.date_from + "', '" + travelInfo.date_to + "', '" + travelInfo.country_from + "', '" + travelInfo.tour_name + "', '" + travelInfo.comment + "');");
+            break;
+        case "foodWith":
+            query = client.query("INSERT INTO travel (userId, type, sex, age, when_from, country_from, city_from, comment) VALUES ('" + userId + "', '" + travelType + "', '" + userInfo.user_gender + "', '" + userInfo.user_age + "', '" + travelInfo.date_from + "', '" + travelInfo.country_from + "', '" + travelInfo.city_from + "', '" + travelInfo.comment + "');");
+            break;
     }
-	
-	res.end();
+
+    res.end();
 });
 
+app.post("/sendLoginInfo", function(req, res) {
+    console.log(req.body);
+    res.send("I got a user info!");
+
+    var id = req.body.userInfo.id;
+    var kakao_nickname = req.body.userInfo.properties.nickname;
+    var kakao_thumbnail = req.body.userInfo.properties.thumbnail_image;
+
+    client.query("INSERT ignore INTO users (Id, name, kakaoThumb) VALUES ('" + parseInt(id) + "', '" + kakao_nickname + "', '" + kakao_thumbnail + "');");
+
+    res.end();
+});
+
+app.get("/getTravelList", function(req, res) {
+    var query = client.query('select * from travel', function(err, rows) {
+        console.log(rows);
+        res.json(rows);
+    });
+    console.log(query);
+});
 
 //var messageFile = __dirname + '/messages.json';
 
 // app.post('/sendMessage', function(req, res) {
-// 	var message = req.body;
-// 	res.send("thank you client!");
+//  var message = req.body;
+//  res.send("thank you client!");
 // });
-// 	pg.connect(connectionString, function(err, client) {
-// 		if(err) {
-// 			return console.error('error fetching client from pool', err);
-// 		}
-// 		console.log(message.message);
-// 		var query = client.query("INSERT INTO messages (msg_id, name, message, time) VALUES (DEFAULT, '" 
-// 							+ JSON.stringify(message.name) + "', '"
-// 							+ JSON.stringify(message.message) + "', '" 
-// 							+ JSON.stringify(message.time) + "');",
-// 			function(err, result) {
-// 				if (err) {
-// 					console.log("ERROR: " + err);
-// 					res.send("ERROR: " + err);
-// 					return;
-// 				}
-// 				console.log("SUCCESS SEND!");
-// 				res.send("success send Message!");
-// 		});
-// 	});
+//  pg.connect(connectionString, function(err, client) {
+//      if(err) {
+//          return console.error('error fetching client from pool', err);
+//      }
+//      console.log(message.message);
+//      var query = client.query("INSERT INTO messages (msg_id, name, message, time) VALUES (DEFAULT, '" 
+//                          + JSON.stringify(message.name) + "', '"
+//                          + JSON.stringify(message.message) + "', '" 
+//                          + JSON.stringify(message.time) + "');",
+//          function(err, result) {
+//              if (err) {
+//                  console.log("ERROR: " + err);
+//                  res.send("ERROR: " + err);
+//                  return;
+//              }
+//              console.log("SUCCESS SEND!");
+//              res.send("success send Message!");
+//      });
+//  });
 // });
 
 // app.get('/getMessages', function(req, res) {
-// 	pg.connect(connectionString, function(err, client) {
-// 		if(err) {
-// 			return console.error('error fetching client from pool', err);
-// 		}
-// 		var query = client.query('SELECT * FROM messages');
-// 		var concatenated = "";
-// 		query.on('row', function(row) {
-// 			console.log(JSON.stringify(row));
-// 			concatenated += JSON.stringify(row);
-// 		});
-// 		query.on('end', function() {
-// 			res.send(concatenated);
-// 			client.end();
-// 		});
-// 	});
+//  pg.connect(connectionString, function(err, client) {
+//      if(err) {
+//          return console.error('error fetching client from pool', err);
+//      }
+//      var query = client.query('SELECT * FROM messages');
+//      var concatenated = "";
+//      query.on('row', function(row) {
+//          console.log(JSON.stringify(row));
+//          concatenated += JSON.stringify(row);
+//      });
+//      query.on('end', function() {
+//          res.send(concatenated);
+//          client.end();
+//      });
+//  });
 // });
 
 
