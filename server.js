d93c5e0fde1bd2f3e31a80236437692549bd6425
jkang107 <jkang107@gmail.com>
2015-05-25 18:37:34 +0900

    cors

--- server.js
+++ server.js
@@ -35,30 +35,6 @@ app.listen(port, function() {
     console.log("Listening on " + port);
 });
 
-/*app.all('/', function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
-  next();
- });*/
-// Enables CORS
-var enableCORS = function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
- 
-    // intercept OPTIONS method
-    if ('OPTIONS' == req.method) {
-      res.send(200);
-    }
-    else {
-      next();
-    }
-};
- 
- 
-// enable CORS!
-app.use(enableCORS);
-
 
 app.get('/read', function(req, res) {
     fs.readFile(file, 'utf8', function(err, data) {
